275c2d9f04773c46c85c7f842ab22550f1640c27
Soren L. Hansen <sorenisanerd@gmail.com>
2023-12-11 11:30:24 -0800

    Add place holders for remaining days

---
+++ y2023/day22/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/sorenisanerd/aoc2023/utils"
+	"gotest.tools/assert"
+)
+
+func TestParts(t *testing.T) {
+	p1, p2 := Parts(sample)
+	assert.Equal(t, 0, p1)
+	assert.Equal(t, 0, p2)
+
+	return
+	p1, p2 = Parts(utils.GetDayData(2023, 22))
+	assert.Equal(t, 0, p1)
+	assert.Equal(t, 0, p2)
+}
---
+++ y2023/day25/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sorenisanerd/aoc2023/utils"
+)
+
+var sample = ``
+
+func main() {
+	fmt.Println(Parts(utils.GetDayData(2023, 25)))
+}
+
+func Parts(s string) (int, int) {
+	return Part(s, false), Part(s, true)
+}
+
+func Part(s string, part2 bool) int {
+	g := utils.ParseGrid[byte](s)
+	fmt.Println(g)
+	return 0
+}
